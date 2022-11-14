afc0b6d7c10a8fc7bbb7b219cf8a88745e5b0ed6
MEHMET SORKULU <112237375+ZorkoMehmet@users.noreply.github.com>
2022-11-14 13:23:18 +0300

    changing-complex-states-practice

--- event-handling-in-react/src/components/App.jsx
+++ event-handling-in-react/src/components/App.jsx
@@ -1,29 +1,65 @@
-import { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [headingName, setHeadingName] = useState("");
-  const [headingSurname, setHeadingSurname] = useState("");
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
 
-  function handleChangeName(event) {
-    setHeadingName(event.target.value);
-  }
-  function handleChangeSurname(event) {
-    setHeadingSurname(event.target.value);
+  function handleChange(event) {
+    const {name, value} = event.target;
+
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email,
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value,
+        };
+      }
+    });
   }
 
   return (
     <div className="container">
       <h1>
-        Hello {headingName} {headingSurname}
+        Hello {contact.fName} {contact.lName}
       </h1>
-      <input onChange={handleChangeName} type="text" placeholder="Name?" />
-      <input
-        onChange={handleChangeSurname}
-        type="text"
-        placeholder="Surname?"
-      />
-      <button>Submit</button>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
