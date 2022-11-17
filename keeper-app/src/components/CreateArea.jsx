85e76d333dcccc85dc17b695dfa1ef717de3141a
MEHMET SORKULU <112237375+ZorkoMehmet@users.noreply.github.com>
2022-11-17 12:05:22 +0300

    before-the-end

---
+++ keeper-app/src/components/CreateArea.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  //   const [newTitle, setNewTitle] = useState("");
+  //   const [newContent, setNewContent] = useState("");
+
+  const [newNote, setNewNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNewNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleClick(event) {
+    props.onAdd(newNote);
+    setNewNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={newNote.title}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={newNote.content}
+        />
+        <button onClick={handleClick}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
